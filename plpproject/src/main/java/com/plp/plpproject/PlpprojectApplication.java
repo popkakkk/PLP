8a62a6cbf621a6ae8c831adf66af76cc21754f2d
poppatipan39 <sir.poppatipan@gmail.com>
2019-01-03 14:13:42 +0700

    initialize my project from spring boots init

---
+++ plpproject/src/main/java/com/plp/plpproject/PlpprojectApplication.java
@@ -0,0 +1,14 @@
+package com.plp.plpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlpprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlpprojectApplication.class, args);
+	}
+
+}
+
